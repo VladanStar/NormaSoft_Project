29fb3b7283c8b898a15fb3d264a0a88bd0878a7f
unknown <vladancupric@gmail.com>
2021-11-19 12:45:22 +0100

    New commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
-import { useEffect, useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import AddPost from './components/AddPost'
+import EditPost from './components/EditPost'
+import Feed from './components/Feed'
+import PostDetails from './components/PostDetails'
+import { PostsContext } from './context/PostsContext'
+
 
 function App() {
   const [postsDogs, setPostsDogs] = useState([]);
-  const postProvider = useMemo(
+  const postsProvider = useMemo(
     () => ({
       postsDogs,
       setPostsDogs,
@@ -13,7 +19,7 @@ function App() {
 
   useEffect(() => {
     // fetching posts for feed
-    const fetchPosts = async () => {
+    const fetchDogs = async () => {
       fetch("https://dummyapi.io/data/v1/post", {
         headers: {
           "app-id": "616ddd0c2a8ff6018ca5b17d",
@@ -25,9 +31,38 @@ function App() {
         });
     };
 
-    fetchPosts();
+    fetchDogs();
   }, []);
-  return <div className="App"></div>;
+  useEffect(() => {
+		window.localStorage.setItem('postsDogs', JSON.stringify(postsDogs))
+	}, [postsDogs])
+
+
+
+	return (
+		<div className="App">
+			<div className="not-feed"></div>
+			<div className="feed flex flex-col p-1 max-w-full">
+				<PostsContext.Provider value={postsProvider}>
+					<BrowserRouter>
+						<Switch>
+							<Route path="/add" component={AddPost} exact />
+							<Route path="/:id" component={PostDetails} exact />
+							<Route
+								path="/:id/edit"
+								component={EditPost}
+								exact
+							/>
+							<Route path="/" component={Feed} exact />
+						</Switch>
+					</BrowserRouter>
+				</PostsContext.Provider>
+			</div>
+			<div className="not-feed"></div>
+		</div>
+	)
 }
+
+
 
 export default App;
